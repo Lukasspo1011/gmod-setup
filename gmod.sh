d0ebe98b7986cf9deb9d8d03da5eca5e57e28901
Lukasspo1011 <lukassporleder@gmx.de>
2025-04-23 21:58:10 +0200

    Update gmod.sh

--- gmod.sh
+++ gmod.sh
@@ -4,7 +4,7 @@
 CTID=120
 HOSTNAME="gmod-server"
 PASSWORD="gmodpass"
-DISK_SIZE="10G"
+DISK_SIZE="10"  # in GB (ohne G)
 MEMORY="2048"
 CORES="2"
 TEMPLATE="local:vztmpl/debian-12-standard_12.2-1_amd64.tar.zst"
@@ -16,8 +16,7 @@ echo "[INFO] Erstelle LXC Container $CTID ($HOSTNAME)..."
 
 pct create $CTID $TEMPLATE \
   --hostname $HOSTNAME \
-  --storage $STORAGE \
-  --rootfs $DISK_SIZE \
+  --rootfs ${STORAGE}:${DISK_SIZE} \
   --cores $CORES \
   --memory $MEMORY \
   --net0 name=eth0,bridge=$BRIDGE,ip=$IP \
@@ -25,13 +24,15 @@ pct create $CTID $TEMPLATE \
   --features nesting=1 \
   --unprivileged 1
 
+echo "[INFO] Starte Container..."
 pct start $CTID
 sleep 5
 
-echo "[INFO] Installiere Garry's Mod im Container..."
+echo "[INFO] Installiere benötigte Pakete im Container..."
 pct exec $CTID -- apt update
 pct exec $CTID -- apt install -y curl wget ca-certificates
 
+echo "[INFO] Führe Installationsskript für Garry's Mod aus..."
 pct exec $CTID -- bash -c "$(curl -fsSL https://raw.githubusercontent.com/Lukasspo1011/gmod-setup/main/install.sh)"
 
 echo "[DONE] Garry's Mod LXC Container wurde erfolgreich eingerichtet!"
