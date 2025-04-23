93c1b8e6d0fed0f427c881c6b2e112ba370410fc
Lukasspo1011 <lukassporleder@gmx.de>
2025-04-23 20:46:11 +0200

    Update gmod.sh

--- gmod.sh
+++ gmod.sh
@@ -1,26 +1,37 @@
 #!/bin/bash
 
+# === Konfiguration ===
 CTID=120
-TEMPLATE="local:vztmpl/debian-12-standard_12.2-1_amd64.tar.zst"
 HOSTNAME="gmod-server"
+PASSWORD="gmodpass"
+DISK_SIZE="10G"
+MEMORY="2048"
+CORES="2"
+TEMPLATE="local:vztmpl/debian-12-standard_12.2-1_amd64.tar.zst"
+STORAGE="local-lvm"
+BRIDGE="vmbr0"
+IP="dhcp"
+
+echo "[INFO] Erstelle LXC Container $CTID ($HOSTNAME)..."
 
 pct create $CTID $TEMPLATE \
   --hostname $HOSTNAME \
-  --cores 2 \
-  --memory 2048 \
-  --rootfs local-lvm:10 \
-  --net0 name=eth0,bridge=vmbr0,ip=dhcp \
-  --password gmodpass \
-  --unprivileged 1 \
-  --features nesting=1
+  --storage $STORAGE \
+  --rootfs $DISK_SIZE \
+  --cores $CORES \
+  --memory $MEMORY \
+  --net0 name=eth0,bridge=$BRIDGE,ip=$IP \
+  --password $PASSWORD \
+  --features nesting=1 \
+  --unprivileged 1
 
 pct start $CTID
-
 sleep 5
 
+echo "[INFO] Installiere Garry's Mod im Container..."
 pct exec $CTID -- apt update
 pct exec $CTID -- apt install -y curl wget ca-certificates
 
-pct exec $CTID -- bash -c "$(curl -fsSL https://raw.githubusercontent.com/lukasspo1011/gmod-setup/main/install.sh)"
+pct exec $CTID -- bash -c "$(curl -fsSL https://raw.githubusercontent.com/Lukasspo1011/gmod-setup/main/install.sh)"
 
-echo "[DONE] Container wurde erstellt und GMod installiert."
+echo "[DONE] Garry's Mod LXC Container wurde erfolgreich eingerichtet!"
